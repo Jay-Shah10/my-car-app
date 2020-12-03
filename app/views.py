f43860805a2c665a7ec9316fd28e1bb3004d19e2
Jay Shah <j24shah@gmail.com>
2020-12-02 21:12:54 -0500

    updated to get back to home page
    this takes it back to mainapp.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from app import app
 from flask import render_template
+from flask import redirect
 
 
-@app.route('/carlist')
+@app.route('/')
 def carlist():
     return render_template('index.html')
+
+@app.route('/mainapp')
+def mainapp():
+   try:
+       return redirect('http://192.168.50.92:5003', code=302)
+   except Exception as e:
+       e
